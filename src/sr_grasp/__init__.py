eafd32187ef464794dc4168c79d4ed41146b2122
Mark Pitchless <markpitchless@gmail.com>
2015-01-05 13:37:26 +0000

    Impliment a yaml file for grasp storage.

--- src/sr_grasp/__init__.py
+++ src/sr_grasp/__init__.py
@@ -1,5 +1,8 @@
 
 from copy import deepcopy
+import yaml
+from rospy import logerr, loginfo
+import genpy
 import moveit_msgs.msg
 from sr_grasp.utils import mk_grasp
 
@@ -17,14 +20,14 @@ class Grasp(moveit_msgs.msg.Grasp):
         """
         grasp = Grasp()
         grasp.id = msg.id
-        pre_grasp_posture
-        grasp.grasp_posture = deepcopy(msg.grasp_posture)
-        grasp.grasp_pose = deepcopy(msg.grasp_pose)
-        grasp.grasp_quality = msg.grasp_quality
+        grasp.pre_grasp_posture  = deepcopy(msg.pre_grasp_posture)
+        grasp.grasp_posture      = deepcopy(msg.grasp_posture)
+        grasp.grasp_pose         = deepcopy(msg.grasp_pose)
+        grasp.grasp_quality      = msg.grasp_quality
         grasp.pre_grasp_approach = deepcopy(msg.pre_grasp_approach)
         grasp.post_grasp_retreat = deepcopy(msg.post_grasp_retreat)
         grasp.post_place_retreat = deepcopy(msg.post_place_retreat)
-        grasp.max_contact_force = msg.max_contact_force
+        grasp.max_contact_force  = msg.max_contact_force
         grasp.allowed_touch_objects = deepcopy(msg.allowed_touch_objects)
         return grasp
 
@@ -34,7 +37,13 @@ class Grasp(moveit_msgs.msg.Grasp):
         """
         raise Exception("TODO - to_msg")
 
-# Dummy inline store for now, will become, params, file, db etc...
+    @classmethod
+    def from_yaml(self, y):
+        grasp = Grasp()
+        genpy.message.fill_message_args(grasp, y)
+        return grasp
+
+# Dummy inline store for now, will become - params, file, db etc...
 _store = {}
 
 class GraspStash(object):
@@ -59,6 +68,10 @@ class GraspStash(object):
         Grasp()
         return Grasp;
 
+    def size(self):
+        """Return the number of grasps."""
+        return len(_store)
+
     def put_grasp(self, grasp):
         """
         Stash the given grasp, using it's id field, which must be set.
@@ -69,3 +82,26 @@ class GraspStash(object):
         #if isinstance(grasp, moveit_msgs.msg.Grasp):
         #    grasp = Grasp.from_msg(grasp)
         _store[grasp.id] = grasp
+
+    def load_yaml_file(self, fname):
+        """
+        Load a set of grasps from a YAML file.
+        """
+        try:
+            data = yaml.load(file(fname))
+            self.load_yaml(data)
+        except Exception as e:
+            logerr("Failed to load YAML grasp file: %s error:%s"%(fname, e))
+            return False
+        else:
+            loginfo("Loaded grasps from file: %s"%(fname))
+            return True
+
+    def load_yaml(self, data):
+        """
+        Load a set of grasps from a YAML object. Throws exceptions on errors.
+        """
+        for g in data:
+            grasp = Grasp.from_yaml(g)
+            self.put_grasp(grasp)
+
