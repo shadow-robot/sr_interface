79f2bc45567701446a20e59a36587b967a091bab
Ugo Cupcic <ugo@shadowrobot.com>
2015-04-08 08:49:11 +0200

    trivial

---
+++ src/sr_robot_commander/sr_robot_commander.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# Copyright 2014 Shadow Robot Company Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import rospy
+
+
+class SrRobotCommander(object):
+    """
+    Class for providing a high level interface to robots.
+    """
+    def __init__(self):
