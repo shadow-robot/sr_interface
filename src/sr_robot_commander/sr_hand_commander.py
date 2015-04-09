41bd3f269744c6804a75ec9edc4cc125272a797b
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-04-09 10:41:53 +0300

    Added deprectation warning to old hand class and skeleton of the new class

---
+++ src/sr_robot_commander/sr_hand_commander.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+# Copyright 2015 Shadow Robot Company Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from sr_robot_commander.sr_robot_commander import SrRobotCommander
+
+
+class SrHandCommander(SrRobotCommander):
+    """
+    Commander class for hand
+    """
+
+    def __init__(self, name="right_hand"):
+        """
+        Initialize object
+        @param name - name of the MoveIt group
+        """
+        super(SrHandCommander, self).__init__(name)
+
+    def get_joint_effort(self):
+        """
+        Returns a dictionary with the effort of each joint. Currently in ADC units, as no calibration is performed on
+        the strain gauges.
+        """
+        raise Exception("Not implemented yet")
+
+    def set_max_force(self, value):
+        """
+        Set maximum force for hand
+        @param value - maximum force value
+        """
+        raise Exception("Not implemented yet")
+
+    def get_tactile_type(self):
+        """
+        Returns a string indicating the type of tactile sensors present. Possible values are: PST, biotac, UBI0 .
+        """
+        raise Exception("Not implemented yet")
+
+    def get_tactile_state(self):
+        """
+        Returns an object containing tactile data. The structure of the data is different for every tactile_type .
+        """
+        raise Exception("Not implemented yet")
