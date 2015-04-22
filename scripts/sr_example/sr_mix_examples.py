81a11a8cfe642914dfe9a89a4a28f7d12d13fb92
Toni Oliver <toni.oliver.d@gmail.com>
2015-04-22 16:27:21 +0100

    Uncomment some commands

--- scripts/sr_example/sr_mix_examples.py
+++ scripts/sr_example/sr_mix_examples.py
@@ -10,16 +10,11 @@ rospy.init_node("basic_arm_examples", anonymous=True)
 hand_commander = SrHandCommander()
 arm_commander = SrArmCommander()
 
-#rospy.sleep(rospy.Duration(2))
-
+# Move hand
 named_target_1 = "open"
 print("Moving to hand named target " + named_target_1)
 hand_commander.move_to_named_target(named_target_1, False)
 
-#position_1 = [-0.028412, 0.17665, 0.85672]
-#position_1 = [-0.128412, 0.27665, 0.85672]
-#print("Moving arm to position\n" + str(position_1) + "\n")
-#arm_commander.move_to_position_target(position_1)
 
 joint_states_1 = {'ra_shoulder_pan_joint': 0.43221632746577665, 'ra_elbow_joint': 2.118891128999479,
                    'ra_wrist_1_joint': -1.711370650686752, 'ra_wrist_2_joint': 1.4834244535003318,
@@ -34,36 +29,32 @@ joint_states = joint_states_1
 print("Moving arm to joint states\n" + str(joint_states) + "\n")
 arm_commander.move_to_joint_value_target(joint_states)
 
+named_target_1 = "pack"
+print("Moving to hand named target " + named_target_1)
+hand_commander.move_to_named_target(named_target_1, True)
+ 
 
-# 
-# 
-# named_target_1 = "pack"
-# print("Moving to hand named target " + named_target_1)
-# hand_commander.move_to_named_target(named_target_1, True)
-# 
-# #rospy.sleep(10.0)
-# 
-# 
-# hand_joint_states_1 = {'rh_FFJ1': 0.35, 'rh_FFJ2': 0.0, 'rh_FFJ3': 0.0, 'rh_FFJ4': 0.0,
-#                        'rh_MFJ1': 0.35, 'rh_MFJ2': 0.0, 'rh_MFJ3': 0.0, 'rh_MFJ4': 0.0,
-#                        'rh_RFJ1': 0.35, 'rh_RFJ2': 0.0, 'rh_RFJ3': 0.0, 'rh_RFJ4': 0.0,
-#                        'rh_LFJ1': 0.35, 'rh_LFJ2': 0.0, 'rh_LFJ3': 0.0, 'rh_LFJ4': 0.0,
-#                        'rh_THJ1': 0.35, 'rh_THJ2': 0.0, 'rh_THJ3': 0.0, 'rh_THJ4': 0.0, 'rh_THJ5': 0.0,
-#                        'rh_WRJ1': 0.0, 'rh_WRJ2': 0.0}
-# hand_joint_states_2 = {'rh_MFJ1': 0.35, 'rh_MFJ2': 0.0, 'rh_MFJ3': 0.0}
-# 
-# # Move hand
-# joint_states = hand_joint_states_2
-# print("Moving hand to joint states\n" + str(joint_states) + "\n")
-# hand_commander.move_to_joint_value_target(joint_states, True)
-# 
-# # Move arm
-# joint_states = joint_states_2
-# print("Moving arm to joint states\n" + str(joint_states) + "\n")
-# arm_commander.move_to_joint_value_target(joint_states)
-# 
-# 
-# #rospy.sleep(5.0)
+ 
+hand_joint_states_1 = {'rh_FFJ1': 0.35, 'rh_FFJ2': 0.0, 'rh_FFJ3': 0.0, 'rh_FFJ4': 0.0,
+                       'rh_MFJ1': 0.35, 'rh_MFJ2': 0.0, 'rh_MFJ3': 0.0, 'rh_MFJ4': 0.0,
+                       'rh_RFJ1': 0.35, 'rh_RFJ2': 0.0, 'rh_RFJ3': 0.0, 'rh_RFJ4': 0.0,
+                       'rh_LFJ1': 0.35, 'rh_LFJ2': 0.0, 'rh_LFJ3': 0.0, 'rh_LFJ4': 0.0,
+                       'rh_THJ1': 0.35, 'rh_THJ2': 0.0, 'rh_THJ3': 0.0, 'rh_THJ4': 0.0, 'rh_THJ5': 0.0,
+                       'rh_WRJ1': 0.0, 'rh_WRJ2': 0.0}
+hand_joint_states_2 = {'rh_MFJ1': 0.35, 'rh_MFJ2': 0.0, 'rh_MFJ3': 0.0}
+ 
+# Move hand
+joint_states = hand_joint_states_2
+print("Moving hand to joint states\n" + str(joint_states) + "\n")
+hand_commander.move_to_joint_value_target(joint_states, True)
+ 
+# Move arm
+joint_states = joint_states_2
+print("Moving arm to joint states\n" + str(joint_states) + "\n")
+arm_commander.move_to_joint_value_target(joint_states)
+ 
+ 
+#rospy.sleep(5.0)
 
 
 
