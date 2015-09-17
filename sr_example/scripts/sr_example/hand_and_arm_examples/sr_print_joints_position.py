9f0f7eb9a93ad8da66a795ce5ea38897f3313d50
Kirsty Ellis <kirsty@shadowrobot.com>
2015-09-17 14:36:17 +0100

    first set of cleanup changes

---
+++ sr_example/scripts/sr_example/hand_and_arm_examples/sr_print_joints_position.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# An example of how to print the current joint positions for all joints.
+# Useful when used in conjunction with 'teach mode', as new motion sequences can
+# be quickly produced from physically moving the robot into position and then
+# recording those positions with this script
+
+import rospy
+from sr_robot_commander.sr_hand_commander import SrHandCommander
+from sr_utilities.hand_finder import HandFinder
+
+
+rospy.init_node("print_joints_position", anonymous=True)
+
+# Use the hand finder to get the hand prefix, to allow this script to be used with either left or right hands
+hand_finder = HandFinder()
+hand_parameters = hand_finder.get_hand_parameters()
+prefix = hand_parameters.mapping.values()[0]
+hand_serial = hand_parameters.mapping.keys()[0]
+
+hand_commander = SrHandCommander(hand_parameters=hand_parameters, hand_serial=hand_serial)
+
+print("Joints positions")
+
+all_joints_state = hand_commander.get_joints_position()
+
+hand_joints_state = {
+    k: v for k, v in all_joints_state.items() if k.startswith(prefix[0] + "_")}
+arm_joints_state = {
+    k: v for k, v in all_joints_state.items() if k.startswith(prefix[0][0] + "a_")}
+
+
+rospy.loginfo("Hand joints position \n " + str(hand_joints_state) + "\n")
+
+rospy.loginfo("Arm joints position \n " + str(arm_joints_state) + "\n")
---
+++ sr_example/scripts/sr_example/hand_examples/sr_store_left_hand.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Script to move the left hand into store position.
+
+import rospy
+from sr_robot_commander.sr_hand_commander import SrHandCommander
+
+
+rospy.init_node("store_left_hand", anonymous=True)
+
+hand_commander = SrHandCommander(name="left_hand", prefix="lh")
+
+open_hand = {'lh_FFJ1': 0.0, 'lh_FFJ2': 0.0, 'lh_FFJ3': 0.0, 'lh_FFJ4': 0.0,
+             'lh_MFJ1': 0.0, 'lh_MFJ2': 0.0, 'lh_MFJ3': 0.0, 'lh_MFJ4': 0.0,
+             'lh_RFJ1': 0.0, 'lh_RFJ2': 0.0, 'lh_RFJ3': 0.0, 'lh_RFJ4': 0.0,
+             'lh_LFJ1': 0.0, 'lh_LFJ2': 0.0, 'lh_LFJ3': 0.0, 'lh_LFJ4': 0.0, 'lh_LFJ5': 0.0,
+             'lh_THJ1': 0.0, 'lh_THJ2': 0.0, 'lh_THJ3': 0.0, 'lh_THJ4': 0.0, 'lh_THJ5': 0.0}
+
+pack_hand = {'lh_FFJ1': 1.5707, 'lh_FFJ2': 1.5707, 'lh_FFJ3': 1.5707, 'lh_FFJ4': 0.0,
+             'lh_MFJ1': 1.5707, 'lh_MFJ2': 1.5707, 'lh_MFJ3': 1.5707, 'lh_MFJ4': 0.0,
+             'lh_RFJ1': 1.5707, 'lh_RFJ2': 1.5707, 'lh_RFJ3': 1.5707, 'lh_RFJ4': 0.0,
+             'lh_LFJ1': 1.5707, 'lh_LFJ2': 1.5707, 'lh_LFJ3': 1.5707, 'lh_LFJ4': 0.0, 'lh_LFJ5': 0.0,
+             'lh_THJ1': 0.35, 'lh_THJ2': 0.58, 'lh_THJ3': 0.0, 'lh_THJ4': 0.39, 'lh_THJ5': 0.25}
+
+
+# Move hand to open position
+joint_states = open_hand
+rospy.loginfo("Moving hand to open position")
+hand_commander.move_to_joint_value_target_unsafe(joint_states, 2.0, False)
+rospy.sleep(2)
+
+# Move hand to closed position
+joint_states = pack_hand
+rospy.loginfo("Moving hand to pack position")
+hand_commander.move_to_joint_value_target_unsafe(joint_states, 2.0, True)
+rospy.sleep(2)
