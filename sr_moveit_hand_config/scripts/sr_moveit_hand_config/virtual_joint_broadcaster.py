3cac32ff3d5d43d64a9d42ad4bb39e9ea8de0425
Beatriz Leon <beatriz@shadowrobot.com>
2015-11-11 13:35:59 +0000

    Improved generation of virtual joint: only when the world link do not exist

--- sr_moveit_hand_config/scripts/sr_moveit_hand_config/virtual_joint_broadcaster.py
+++ sr_moveit_hand_config/scripts/sr_moveit_hand_config/virtual_joint_broadcaster.py
@@ -8,13 +8,15 @@ from sr_utilities.local_urdf_parser_py import URDF
 
 
 def publish_world_to_base_transform():
-    rospy.init_node('virtual_joint_broadcaster', anonymous=True)
-
-    urdf_str = rospy.get_param('robot_description')
+    while not rospy.has_param('/robot_description'):
+        rospy.sleep(0.5)
+        rospy.loginfo("waiting for robot_description")
+    urdf_str = rospy.get_param('/robot_description')
+    robot_urdf = URDF.from_xml_string(urdf_str)
     robot = URDF.from_xml_string(urdf_str)
     robot_root = robot.get_root()
 
-    if robot_root is not None:
+    if robot_root is not None and robot_root != "world":
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             br = tf.TransformBroadcaster()
@@ -23,6 +25,7 @@ def publish_world_to_base_transform():
             rate.sleep()
 
 if __name__ == '__main__':
+    rospy.init_node('virtual_joint_broadcaster', anonymous=True)
     # Publish world to base transform
     try:
         publish_world_to_base_transform()
