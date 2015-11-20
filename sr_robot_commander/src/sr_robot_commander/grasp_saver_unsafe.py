8f89844724ac249781ae2e2995d37bef6626ae80
dg <dg@shadowrobot.com>
2015-11-20 17:14:06 +0000

    changed name, added disclaimer + instructions

--- sr_robot_commander/src/sr_robot_commander/grasp_saver_unsafe.py
+++ sr_robot_commander/src/sr_robot_commander/grasp_saver_unsafe.py
@@ -10,8 +10,35 @@ from sr_robot_commander.sr_arm_commander import SrArmCommander
 from sr_robot_commander.sr_hand_commander import SrHandCommander
 from sr_utilities.hand_finder import HandFinder
 
+"""
+***DISCLAIMER***
 
-class SrCrudeGraspSaver:
+This script provides a command line interface to save the current pose of the robot (made of an arm and hand).
+Poses can be composed of either hand joints/arm joints/all joints. This can be useful for making smooth
+movements where the hand and arm should move synchronously. However, there are no safeties! In particular,
+if a hand and arm aren't both present , its behaviour is undefined. Likewise if more than one hand/arm are
+present. There is also no checking to prevent overwriting existing database entries/otherwise damaging the
+robot_states database. It is included here as it is a useful tool, but it should be considered a work in
+progress and used with care.
+
+You have been warned :)
+
+
+To use this script:
+
+rosrun sr_robot_commander grasp_saver_unsafe.py NAME_TO_SAVE WHAT_TO_SAVE
+
+NAME_TO_SAVE is the name that will be used for the state in the database.
+
+N.B. IF A STATE ALREADY EXISTS WITH THAT NAME IT WILL BE OVERWRITTEN WITH NO WARNING!
+
+WHAT_TO_SAVE specifies which joints will be included. It can be "arm","hand" or "both".
+
+If WHAT_TO_SAVE is omitted, it defaults to "both".
+"""
+
+
+class SrGraspSaverUnsafe:
     def __init__(self, name, hand_or_arm="both"):
 
         self.__save = rospy.ServiceProxy(
@@ -68,5 +95,5 @@ if "__main__" == __name__:
     if len(argv) > 2:
         which = argv[2]
 
-    gs = SrCrudeGraspSaver(argv[1], which)
+    gs = SrGraspSaverUnsafe(argv[1], which)
     gs.spin()
