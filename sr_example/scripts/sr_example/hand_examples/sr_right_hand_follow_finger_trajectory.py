5ef6e1bf07ca1dc6cd828a6ba69d7722fcc8142e
Beatriz Leon <beatriz@shadowrobot.com>
2015-11-30 11:26:19 +0000

    example changed to use different frame and fixed planning option

--- sr_example/scripts/sr_example/hand_examples/sr_right_hand_follow_finger_trajectory.py
+++ sr_example/scripts/sr_example/hand_examples/sr_right_hand_follow_finger_trajectory.py
@@ -15,60 +15,74 @@ hand_parameters = hand_finder.get_hand_parameters()
 prefix = hand_parameters.mapping.values()[0]
 hand_serial = hand_parameters.mapping.keys()[0]
 
-# Open fingers
-group_id = prefix + "_fingers"
-hand_commander = SrHandCommander(name=group_id)
-hand_commander.move_to_named_target("fingers_open")
+# Close all fingers except thumb
+hand_commander = SrHandCommander()
+hand_commander.move_to_named_target("fingers_pack_thumb_open")
 
+# Change to first finger group
 group_id = prefix + "_first_finger"
 hand_commander = SrHandCommander(name=group_id)
 
-rospy.sleep(2)
+# Open first finger
+hand_commander.move_to_named_target("first_finger_open")
+
+rospy.sleep(4)
+
 rospy.loginfo("\nMoving Index finger following a set of waypoints")
 
 waypoints = []
 
+# Move horizontally
+start_pose = geometry_msgs.msg.Pose()
+start_pose.position.x = 0.034
+start_pose.position.y = -0.002
+start_pose.position.z = 0.197
+start_pose.orientation.w = 1.00
+waypoints.append(start_pose)
+
 pose = geometry_msgs.msg.Pose()
-pose.position.x = 0.057038
-pose.position.y = -0.13281
-pose.position.z = 0.37172
-pose.orientation.x = 0.60863
-pose.orientation.y = 0.05909
-pose.orientation.z = -0.044238
-pose.orientation.w = 0.79001
+pose.position.x = 0.067
+pose.position.y = -0.010
+pose.position.z = 0.184
+pose.orientation.w = 1.00
+waypoints.append(pose)
+
+waypoints.append(start_pose)
+
+pose = geometry_msgs.msg.Pose()
+pose.position.x = 0.011
+pose.position.y = -0.014
+pose.position.z = 0.187
+pose.orientation.w = 1.00
+waypoints.append(pose)
+
+waypoints.append(start_pose)
+
+# Move vertically
+pose = geometry_msgs.msg.Pose()
+pose.position.x = 0.033
+pose.position.y = -0.016
+pose.position.z = 0.190
+pose.orientation.w = 1.00
 waypoints.append(pose)
 
 pose = geometry_msgs.msg.Pose()
-pose.position.x = 0.08341
-pose.position.y = -0.13383
-pose.position.z = 0.36473
-pose.orientation.x = 0.55
-pose.orientation.y = 0.23079
-pose.orientation.z = -0.058283
-pose.orientation.w = 0.80052
+pose.position.x = 0.033
+pose.position.y = -0.035
+pose.position.z = 0.178
+pose.orientation.w = 1.00
 waypoints.append(pose)
 
 pose = geometry_msgs.msg.Pose()
-pose.position.x = 0.083263
-pose.position.y = -0.10451
-pose.position.z = 0.39957
-pose.orientation.x = 0.27607
-pose.orientation.y = 0.16801
-pose.orientation.z = -0.0061039
-pose.orientation.w = 0.94632
+pose.position.x = 0.033
+pose.position.y = -0.068
+pose.position.z = 0.151
+pose.orientation.w = 1.00
 waypoints.append(pose)
 
-pose = geometry_msgs.msg.Pose()
-pose.position.x = 0.067446
-pose.position.y = -0.099706
-pose.position.z = 0.40685
-pose.orientation.x = 0.24168
-pose.orientation.y = 0.083281
-pose.orientation.z = -0.021483
-pose.orientation.w = 0.96654
-waypoints.append(pose)
+waypoints.append(start_pose)
 
-hand_commander.plan_to_waypoints_target(waypoints)
+hand_commander.plan_to_waypoints_target(waypoints, reference_frame=prefix + "_palm")
 hand_commander.execute()
 
 rospy.sleep(3.0)
