64357ce9ecc475274d23303216b55633ea4af1c7
Beatriz Leon <beatriz@shadowrobot.com>
2015-12-18 11:55:47 +0000

    Fixed tests

--- sr_multi_moveit/sr_multi_moveit_test/test/test_iksolvers.cpp
+++ sr_multi_moveit/sr_multi_moveit_test/test/test_iksolvers.cpp
@@ -1,15 +1,11 @@
 #include <moveit/move_group_interface/move_group.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
-#include <moveit_msgs/DisplayRobotState.h>
-#include <moveit_msgs/DisplayTrajectory.h>
-
-#include <moveit_msgs/AttachedCollisionObject.h>
-#include <moveit_msgs/CollisionObject.h>
 #include <moveit/planning_scene/planning_scene.h>
 #include <moveit/robot_model_loader/robot_model_loader.h>
 
 #include <eigen_conversions/eigen_msg.h>
+#include <ctime>
 
 class TestIKsolvers
 {
@@ -18,7 +14,6 @@ class TestIKsolvers
 
   public:
   TestIKsolvers();
-    ~TestIKsolvers();
   void AddSceneObjects(void);
   bool TestRandomState(void);
 };
@@ -27,37 +22,24 @@ TestIKsolvers::TestIKsolvers():group("right_arm")
 {
 }
 
-TestIKsolvers::~TestIKsolvers()
-{
-  ros::shutdown();
-}
-
 bool TestIKsolvers::TestRandomState(void)
 {
   robot_state::RobotStatePtr test_state = group.getCurrentState();
 
   group.setStartState(*group.getCurrentState());
   geometry_msgs::PoseStamped random_pose =  group.getRandomPose();
-  //ROS_INFO_STREAM("Pose:"<< random_pose.pose);
 
   found_ik = test_state->setFromIK(group.getCurrentState()->getRobotModel()->getJointModelGroup(group.getName()),
       random_pose.pose, 10, 0.1);
 
-  //ROS_INFO_STREAM("IK solution " << (found_ik ? "found" : "not found"));
-
   if (found_ik)
   {
-    /*moveit::planning_interface::MoveGroup::Plan my_plan;
-    group.setJointValueTarget(*test_state);
-    bool success = group.plan(my_plan);
-    ROS_INFO("Visualizing plan  %s", success?"SUCCEDED":"FAILED");*/
-
     return true;
-
   }
   else
   {
     ROS_INFO("Did not find IK solution");
+    ROS_INFO_STREAM("Pose:"<< random_pose.pose);
     return false;
   }
 }
@@ -69,20 +51,36 @@ int main(int argc, char **argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
   int num_succeeded_positions = 0;
+  double maximum=0.0;
+  double minimum=100;
+
+  double elapsed_secs;
+
+  clock_t global_begin = clock();
 
   TestIKsolvers *testIKsolvers = new TestIKsolvers();
   int total_random_queries = 10000;
   for (std::size_t i = 0; i < total_random_queries; i++)
   {
+    clock_t begin = clock();
     bool succeed = testIKsolvers->TestRandomState();
     if (succeed)
     {
       num_succeeded_positions ++;
     }
+    clock_t end = clock();
+    elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    maximum=(elapsed_secs>maximum)?elapsed_secs:maximum;
+    minimum=(elapsed_secs<minimum)?elapsed_secs:minimum;
   }
+  clock_t global_end = clock();
+  double global_elapsed_secs = double(global_end - global_begin) / CLOCKS_PER_SEC;
   ROS_INFO_STREAM("Number of valid ik plans " << num_succeeded_positions << "/" << total_random_queries);
+  ROS_INFO_STREAM("Total time taken " << global_elapsed_secs);
+  ROS_INFO_STREAM("Average time per test " << global_elapsed_secs/total_random_queries);
+  ROS_INFO_STREAM("Maximum time per test " << maximum);
 
-  ros::waitForShutdown();
+  ros::shutdown();
   return 0;
 }
 
