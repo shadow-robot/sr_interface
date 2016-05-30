0779863283f76f3473c0ec0640cec6e9a6d80c17
Ugo Cupcic <ugo@shadowrobot.com>
2016-05-30 14:52:36 +0200

    moved benchmarking to its own repo - more convenient

--- sr_benchmarking/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['sr_benchmarking'],
-    package_dir={'': 'src'})
-
-setup(**setup_args)
