38d5ca0b8931d9042a3a41f4dfa5a3e78d2da6cc
Ugo Cupcic <ugo@shadowrobot.com>
2016-06-02 11:43:04 +0000

    missing roscore

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@ set -e
 
 # setup ros environment
 source "/workspace/shadow_robot/base/devel/setup.bash"
+
+roscore & 
+
 python /workspace/shadow_robot/base/src/sr_interface/sr_multi_moveit/sr_moveit_planner_benchmarking/src/benchmark_planners.py
