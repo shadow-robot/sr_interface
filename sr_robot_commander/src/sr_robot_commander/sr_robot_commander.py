1f03f967d61a6e854877a0e97b286092f69120fb
giusebar <giuseppe@shadowrobot.com>
2016-11-04 12:32:44 +0000

    Discard changes on robot_commander

--- sr_robot_commander/src/sr_robot_commander/sr_robot_commander.py
+++ sr_robot_commander/src/sr_robot_commander/sr_robot_commander.py
@@ -100,14 +100,8 @@ class SrRobotCommander(object):
 
         threading.Thread(None, rospy.spin)
 
-    def set_planner_id(self, planner_id):
-        self._move_group_commander.set_planner_id(planner_id)
-
     def set_num_planning_attempts(self, num_planning_attempts):
         self._move_group_commander.set_num_planning_attempts(num_planning_attempts)
-
-    def set_planner_id(self, planner_id):
-        self._move_group_commander.set_planner_id(planner_id)
 
     def set_planning_time(self, seconds):
         self._move_group_commander.set_planning_time(seconds)
