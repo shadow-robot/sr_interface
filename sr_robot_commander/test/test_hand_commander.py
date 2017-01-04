ee3a9fdcaf7827aaff1472094c8c205f63924231
Beatriz Leon <beatriz@shadowrobot.com>
2017-01-04 10:39:47 -0500

    even more time

--- sr_robot_commander/test/test_hand_commander.py
+++ sr_robot_commander/test/test_hand_commander.py
@@ -32,7 +32,7 @@ class TestSrHandCommander(TestCase):
         rospy.init_node('test_hand_commander', anonymous=True)
         # Waiting for rviz and moveit to start
         rospy.wait_for_service('/move_group/set_logger_level')
-        rospy.sleep(30)
+        rospy.sleep(200)
 
     def test_strip_prefix(self):
         hand_commander = SrHandCommander()
