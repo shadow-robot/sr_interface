52a644bcfe3f95290173fe5f57334737acf7d5f3
Beatriz Leon <beatriz@shadowrobot.com>
2017-01-11 11:56:09 -0500

    Another test

--- sr_robot_commander/test/test_hand_commander.py
+++ sr_robot_commander/test/test_hand_commander.py
@@ -32,6 +32,7 @@ class TestSrHandCommander(TestCase):
         rospy.init_node('test_hand_commander', anonymous=True)
         # Waiting for rviz and moveit to start
         rospy.wait_for_service('/move_group/set_logger_level')
+        rospy.sleep(500)
 
     def test_strip_prefix(self):
         hand_commander = SrHandCommander()
@@ -47,7 +48,6 @@ class TestSrHandCommander(TestCase):
         self.assertEqual(hand_parameters.mapping[hand_serial], "rh", msg="Hand mapping wrong")
         self.assertEqual(hand_parameters.joint_prefix[hand_serial], "rh_", msg="Prefix wrong")
 
-        rospy.sleep(500)
         hand_commander = SrHandCommander(hand_parameters=hand_parameters, hand_serial=hand_serial)
         rospy.sleep(500)
         self.assertGreater(len(hand_commander.get_joints_position()), 0, "No joints found, init must have failed.")
