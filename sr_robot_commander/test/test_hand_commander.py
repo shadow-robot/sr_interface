8ef778748fa0678373569c6276c227b147704dfc
Beatriz Leon <beatriz@shadowrobot.com>
2017-01-25 12:34:07 -0500

    Revert "Merge branch 'F_fixing_test' into F_use_unified_moveit_repo"
    This reverts commit 263d917a51b3c0368874f6881c1b839c57eec4a3, reversing
    changes made to 19a2d31e8bbb37b25dce3e1f78b4f1fcaa0ca12e.

--- sr_robot_commander/test/test_hand_commander.py
+++ sr_robot_commander/test/test_hand_commander.py
@@ -30,9 +30,6 @@ class TestSrHandCommander(TestCase):
 
     def setUp(self):
         rospy.init_node('test_hand_commander', anonymous=True)
-        # Waiting for rviz and moveit to start
-        rospy.wait_for_service('/move_group/set_logger_level')
-        rospy.sleep(2.0)
 
     def test_strip_prefix(self):
         hand_commander = SrHandCommander()
