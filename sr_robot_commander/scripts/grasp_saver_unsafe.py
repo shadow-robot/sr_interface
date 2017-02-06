1d44e65394616530765bc7c787b1aa793ebcfec4
Toni Oliver <toni@shadowrobot.com>
2017-02-06 15:44:58 +0000

    tested

--- sr_robot_commander/scripts/grasp_saver_unsafe.py
+++ sr_robot_commander/scripts/grasp_saver_unsafe.py
@@ -36,15 +36,15 @@ If WHAT_TO_SAVE is omitted, it defaults to "both".
 
 if "__main__" == __name__:
     rospy.init_node("state_saver")
-    name = rospy.getparam("~name")
-    which = rospy.getparam("~which", "all")
-    hand_h = rospy.getparam("~hand_h", False)
-    save_target = rospy.getparam("~save_target", False)
+    name = rospy.get_param("~name")
+    which = rospy.get_param("~which", "all")
+    hand_h = rospy.get_param("~hand_h", False)
+    save_target = rospy.get_param("~save_target", False)
 
 
     if which == "all":
-        gs = SrStateSaverUnsafe(argv[1] + "_hand", "hand", hand_h=hand_h, save_target=save_target)
-        gs = SrStateSaverUnsafe(argv[1] + "_arm", "arm", hand_h=hand_h, save_target=save_target)
-        gs = SrStateSaverUnsafe(argv[1] + "_both", "both", hand_h=hand_h, save_target=save_target)
+        gs = SrStateSaverUnsafe(name + "_hand", "hand", hand_h=hand_h, save_target=save_target)
+        gs = SrStateSaverUnsafe(name + "_arm", "arm", hand_h=hand_h, save_target=save_target)
+        gs = SrStateSaverUnsafe(name + "_both", "both", hand_h=hand_h, save_target=save_target)
     else:
-        gs = SrStateSaverUnsafe(argv[1] + "_" + which, which, hand_h=hand_h, save_target=save_target)
+        gs = SrStateSaverUnsafe(name + "_" + which, which, hand_h=hand_h, save_target=save_target)
