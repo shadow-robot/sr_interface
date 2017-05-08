2ce4f3c7a0081e5d38a122e9ce7012a5fbe35022
dg <dg@shadowrobot.com>
2017-05-08 15:01:14 +0100

    ready to test

--- sr_robot_commander/src/sr_robot_commander/sr_robot_state_saver.py
+++ sr_robot_commander/src/sr_robot_commander/sr_robot_state_saver.py
@@ -13,7 +13,7 @@ from sr_utilities.hand_finder import HandFinder
 
 
 class SrStateSaverUnsafe(object):
-    def __init__(self, name, hand_or_arm="both", hand_h=False):
+    def __init__(self, name, hand_or_arm="both"):
 
         self.__save = rospy.ServiceProxy(
             'save_robot_state', SaveState)
@@ -26,28 +26,12 @@ class SrStateSaverUnsafe(object):
             self.__commander = SrArmCommander()
 
         elif hand_or_arm == 'hand':
-            if self.__hand_h:
-                self.__commander = SrRobotCommander("hand_h", prefix="H1_")
-            else:
-                hand_finder = HandFinder()
+            self.__commander = SrHandCommander()
 
-                hand_parameters = hand_finder.get_hand_parameters()
-                hand_serial = hand_parameters.mapping.keys()[0]
-
-                self.__commander = SrHandCommander(hand_parameters=hand_parameters, hand_serial=hand_serial)
         else:
             self.__arm_commander = SrArmCommander()
+            self.__hand_commander = SrHandCommander()
 
-            if self.__hand_h:
-                self.__hand_commander = SrRobotCommander("hand_h", prefix="H1_")
-            else:
-                hand_finder = HandFinder()
-
-                hand_parameters = hand_finder.get_hand_parameters()
-                hand_serial = hand_parameters.mapping.keys()[0]
-
-                self.__hand_commander = SrHandCommander(hand_parameters=hand_parameters,
-                                                        hand_serial=hand_serial)
         self.__hand_or_arm = hand_or_arm
 
         rs = RobotState()
