6fcf019a9902fe57303c926d595f25daf567ac73
Beatriz Leon <beatriz@shadowrobot.com>
2017-09-29 15:36:10 +0000

    Removing ikfast for now until we decide to include it

--- sr_multi_moveit/ur10srh_right_arm_moveit_ikfast_plugin/update_ikfast_plugin.sh
+++
@@ -1,1 +0,0 @@
-rosrun moveit_kinematics create_ikfast_moveit_plugin.py ur10srh right_arm ur10srh_right_arm_moveit_ikfast_plugin $(rospack find ur10srh_right_arm_moveit_ikfast_plugin)/src/ur10srh_right_arm_ikfast_solver.cpp
