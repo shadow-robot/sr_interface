382d59e07ea49a50c46ffc8b0c16d9b55cd46dfb
dg-shadow <dg@shadowrobot.com>
2018-07-02 15:12:00 +0000

    comments

--- sr_robot_commander/test/test_state_saver_no_hardware.py
+++ sr_robot_commander/test/test_state_saver_no_hardware.py
@@ -85,5 +85,5 @@ class TestSrStateSaverExceptions(TestCase):
 
 
 if __name__ == "__main__":
-    import rostest
-    rostest.rosrun(PKG, "test_state_saver_no_hardware", TestSrStateSaverExceptions)
+    import rosunit
+    rosunit.rosrun(PKG, "test_state_saver_no_hardware", TestSrStateSaverExceptions)
