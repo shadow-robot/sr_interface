52ae82f57a17c1c3fe60858fcfb7e65d3ad7f6e4
Your Name <you@example.com>
2019-06-07 10:41:58 +0000

    update

--- sr_multi_moveit/sr_multi_moveit_config/scripts/generate_moveit_config.py
+++ sr_multi_moveit/sr_multi_moveit_config/scripts/generate_moveit_config.py
@@ -333,13 +333,17 @@ def generate_kinematics(robot, robot_config, hand_template_path="kinematics_temp
 
         if manipulator.has_hand:
             # open hand template files
-            # tracik_template_path = (hand_template_path[0:hand_template_path.find("_template")] +
-            #                         "_tracik_template.yaml")
             with open(hand_template_path, 'r') as stream:
                 hand_yamldoc = yaml.load(stream)
-            # with open(tracik_template_path, 'r') as stream:
-            #     hand_yamldock_tracik = yaml.load(stream)
-            hand_yamldock_tracik = deepcopy(hand_yamldoc)
+
+            if 'tracik' not in hand_template_path and 'bioik' not in hand_template_path:
+                fixed_joint_template_path = (hand_template_path[0:hand_template_path.find("_template")] +
+                                        "_tracik_template.yaml")
+
+                with open(fixed_joint_template_path, 'r') as stream:
+                    hand_yamldock_fixed_joint = yaml.load(stream)
+            else:
+                hand_yamldock_fixed_joint = deepcopy(hand_yamldoc)
 
             prefix = manipulator.hand.prefix
             finger_prefixes = ["FF", "MF", "RF", "LF", "TH"]
@@ -372,7 +376,7 @@ def generate_kinematics(robot, robot_config, hand_template_path="kinematics_temp
 
                 if group_name in hand_yamldoc and group_prefix == prefix:
                     if is_fixed.get(group_name):
-                        kinematics_config = hand_yamldock_tracik[group_name]
+                        kinematics_config = hand_yamldock_fixed_joint[group_name]
                     else:
                         kinematics_config = hand_yamldoc[group_name]
 
