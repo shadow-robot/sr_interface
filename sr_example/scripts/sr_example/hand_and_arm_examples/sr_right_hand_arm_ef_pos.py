b91828626582474b10e3a7ee76659136faa5bf63
georgiablanco <georgia@shadowrobot.com>
2020-10-20 16:05:46 +0000

    updating sr_examples

--- sr_example/scripts/sr_example/hand_and_arm_examples/sr_right_hand_arm_ef_pos.py
+++ sr_example/scripts/sr_example/hand_and_arm_examples/sr_right_hand_arm_ef_pos.py
@@ -30,6 +30,8 @@
 # simulated robot:
 #     roslaunch sr_right_ur10arm_hand.launch sim:=true scene:=true
 
+# It is recommended to run this script in simulation first.
+
 import rospy
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from sr_robot_commander.sr_robot_commander import SrRobotCommander
