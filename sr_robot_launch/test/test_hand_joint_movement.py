979e96baf75b00e6ef8d089792929d7131f67260
georgiablanco <georgia@shadowrobot.com>
2020-11-04 09:22:19 +0000

    adding a new hand test script and test launch file

---
+++ sr_robot_launch/test/test_hand_joint_movement.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+# Copyright 2015 Shadow Robot Company Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import rospy
+import rostest
+from sr_robot_commander.sr_hand_commander import SrHandCommander
+from actionlib_msgs.msg import GoalStatusArray
+from unittest import TestCase
+# from config import ConfigPack
+
+PKG = "sr_robot_launch"
+hand_type = rospy.get_param('/test_hand_joint_movement/test_sim/hand_type', "hand_e")
+hand_id = rospy.get_param('/test_hand_joint_movement/test_sim/hand_id', "rh")
+
+ConfigPack = {
+    'hand_e': {'THJ1': 0.52, 'THJ2': 0.61, 'THJ3': 0.0, 'THJ4': 1.20, 'THJ5': 0.17, 
+               'FFJ1': 1.5707, 'FFJ2': 1.5707, 'FFJ3': 1.5707, 'FFJ4': 0.0,
+               'MFJ1': 1.5707, 'MFJ2': 1.5707, 'MFJ3': 1.5707, 'MFJ4': 0.0,
+               'RFJ1': 1.5707, 'RFJ2': 1.5707, 'RFJ3': 1.5707, 'RFJ4': 0.0,
+               'LFJ1': 1.5707, 'LFJ2': 1.5707, 'LFJ3': 1.5707, 'LFJ4': 0.0, 
+               'LFJ5': 0.0, 'WRJ1': 0.0, 'WRJ2': 0.0}, 
+    'hand_e_plus': {'THJ1': 0.52, 'THJ2': 0.61, 'THJ3': 0.0, 'THJ4': 1.20, 'THJ5': 0.17, 
+               'FFJ1': 1.5707, 'FFJ2': 1.5707, 'FFJ3': 1.5707, 'FFJ4': 0.0,
+               'MFJ1': 1.5707, 'MFJ2': 1.5707, 'MFJ3': 1.5707, 'MFJ4': 0.0,
+               'RFJ1': 1.5707, 'RFJ2': 1.5707, 'RFJ3': 1.5707, 'RFJ4': 0.0,
+               'LFJ1': 1.5707, 'LFJ2': 1.5707, 'LFJ3': 1.5707, 'LFJ4': 0.0, 
+               'LFJ5': 0.0, 'WRJ1': 0.0, 'WRJ2': 0.0}, 
+    'hand_lite': {'THJ1': 0.52, 'THJ2': 0.61, 'THJ4': 1.20, 'THJ5': 0.17, 
+                  'FFJ1': 1.5707, 'FFJ2': 1.5707, 'FFJ3': 1.5707, 'FFJ4': 0.0,
+                  'MFJ1': 1.5707, 'MFJ2': 1.5707, 'MFJ3': 1.5707, 'MFJ4': 0.0,
+                  'RFJ1': 1.5707, 'RFJ2': 1.5707, 'RFJ3': 1.5707, 'RFJ4': 0.0}, 
+    'hand_extra_lite': {'THJ1': 0.52, 'THJ2': 0.61, 'THJ4': 1.20, 'THJ5': 0.17, 
+                        'FFJ1': 1.5707, 'FFJ2': 1.5707, 'FFJ3': 1.5707, 'FFJ4': 0.0,
+                        'RFJ1': 1.5707, 'RFJ2': 1.5707, 'RFJ3': 1.5707, 'RFJ4': 0.0}
+}
+
+class TestHandJointMovement(TestCase):
+    """
+    Tests the Hand Commander
+    """
+    @classmethod
+    def setUpClass(cls):
+        rospy.loginfo("This is the hand type %s", hand_type)
+        rospy.init_node('test_sim', anonymous=True)
+        rospy.wait_for_message('/move_group/status', GoalStatusArray)
+        if hand_id == 'rh':
+            cls.hand_commander = SrHandCommander(name='right_hand')
+        if hand_id == 'lh':
+            cls.hand_commander = SrHandCommander(name='left_hand')
+        else:
+            rospy.logerr("Hand side not specified correctly")
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_hand_open(self):
+        open_joints_target = self.hand_commander.get_joints_position()
+
+        for k in open_joints_target:
+            open_joints_target[k] = 0.0
+
+        self.hand_commander.move_to_joint_value_target(open_joints_target, wait=True)
+        rospy.sleep(10)
+        final_joint_values = self.hand_commander.get_current_state()
+
+        expected_and_final_joint_value_diff = 0
+        for expected_value, final_value in zip(open_joints_target, final_joint_values):
+            expected_and_final_joint_value_diff += abs(open_joints_target[expected_value] -
+                                                       final_joint_values[final_value])
+
+        self.assertAlmostEqual(expected_and_final_joint_value_diff, 0, places=1)
+
+    def test_hand_pack(self):
+        joints_target = ConfigPack[hand_type]
+
+        pack_joints_target = {}
+        if hand_id == 'rh':
+            for k, v in joints_target.items():
+                pack_joints_target['rh_' + k] = v
+        if hand_id == 'lh':
+            for k, v in joints_target.items():
+                pack_joints_target['lh_' + k] = v
+
+        self.hand_commander.move_to_joint_value_target(pack_joints_target, wait=True)
+        rospy.sleep(10)
+        final_joint_values = self.hand_commander.get_current_state()
+
+        expected_and_final_joint_value_diff = 0
+        for expected_value, final_value in zip(pack_joints_target, final_joint_values):
+            expected_and_final_joint_value_diff += abs(pack_joints_target[expected_value] -
+                                                       final_joint_values[final_value])
+
+        self.assertAlmostEqual(expected_and_final_joint_value_diff, 0, places=1)
+
+
+if __name__ == "__main__":
+    rostest.rosrun(PKG, "test_sim", TestHandJointMovement)
