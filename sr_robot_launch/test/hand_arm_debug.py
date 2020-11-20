333cb59931e2ce235256d0799cee8b4f2a5f5913
Joanna Phillips <joanna@shadowrobot.com>
2020-11-20 14:34:09 +0000

    arm and hand tested together

--- sr_robot_launch/test/hand_arm_debug.py
+++ sr_robot_launch/test/hand_arm_debug.py
@@ -28,9 +28,9 @@ class TestHandAndArmSim(TestCase):
     Tests the Hand and Arm in Sim
     """
     def __init__(self):
-        self.launch_file = 'sr_left_ur10arm_hand.launch'
+#        self.launch_file = 'sr_left_ur10arm_hand.launch'
 #        self.launch_file = 'sr_left_ur5arm_hand.launch'
-#        self.launch_file = 'sr_right_ur10arm_hand.launch'
+        self.launch_file = 'sr_right_ur10arm_hand.launch'
 #        self.launch_file = 'sr_right_ur5arm_hand.launch'
         if 'ur10' in self.launch_file:
             self.hand_type = 'hand_e'
@@ -51,15 +51,16 @@ class TestHandAndArmSim(TestCase):
 
         rospy.wait_for_message('/move_group/status', GoalStatusArray)
         if self.hand_id == 'rh':
-#            self.robot_commander = SrRobotCommander(name="right_arm_and_hand")
+            self.robot_commander = SrRobotCommander(name="right_arm_and_hand")
             self.hand_commander = SrHandCommander(name='right_hand')
             self.arm_commander = SrArmCommander(name='right_arm')
         elif self.hand_id == 'lh':
-#            self.robot_commander = SrRobotCommander(name="left_arm_and_hand")
+            self.robot_commander = SrRobotCommander(name="left_arm_and_hand")
             self.hand_commander = SrHandCommander(name='left_hand')
             self.arm_commander = SrArmCommander(name='left_arm')
 
     def joints_error_check(self, expected_joint_values, recieved_joint_values):
+        expected_and_final_joint_value_diff = 0
         for expected_value, recieved_value in zip(sorted(expected_joint_values), sorted(recieved_joint_values)):
             expected_and_final_joint_value_diff += abs(expected_joint_values[expected_value] -
                                                        recieved_joint_values[recieved_value])
@@ -118,11 +119,57 @@ class TestHandAndArmSim(TestCase):
         print(expected_and_final_joint_value_diff_arm)
 
         self.assertAlmostEqual(expected_and_final_joint_value_diff_arm, 0, delta=0.2)
+    
+    def test_hand_and_arm(self):
+        hand_joints_target = {
+           'hand_e': {'THJ1': 0.52, 'THJ2': 0.61, 'THJ3': 0.0, 'THJ4': 1.20, 'THJ5': 0.17,
+                            'FFJ1': 1.5707, 'FFJ2': 1.5707, 'FFJ3': 1.5707, 'FFJ4': 0.0,
+                            'MFJ1': 1.5707, 'MFJ2': 1.5707, 'MFJ3': 1.5707, 'MFJ4': 0.0,
+                            'RFJ1': 1.5707, 'RFJ2': 1.5707, 'RFJ3': 1.5707, 'RFJ4': 0.0,
+                            'LFJ1': 0.0, 'LFJ2': 0.0, 'LFJ3': 0.0, 'LFJ4': 0.0,
+                            'LFJ5': 0.0, 'WRJ1': 0.0, 'WRJ2': 0.0},
+           'hand_lite': {'THJ1': 0.52, 'THJ2': 0.61, 'THJ4': 1.20, 'THJ5': 0.17,
+                          'FFJ1': 1.5707, 'FFJ2': 1.5707, 'FFJ3': 1.5707, 'FFJ4': 0.0,
+                          'MFJ1': 1.5707, 'MFJ2': 1.5707, 'MFJ3': 1.5707, 'MFJ4': 0.0,
+                          'RFJ1': 0.0, 'RFJ2': 0.0, 'RFJ3': 0.0, 'RFJ4': 0.0},
+                }
+
+        hand_joints_target_no_id = hand_joints_target[self.hand_type]
+        hand_joints_target = {}
+        for key, value in hand_joints_target_no_id.items():
+            hand_joints_target[self.hand_id + '_' + key] = value
+
+        arm_joints_target = {'shoulder_pan_joint': 0.00, 'elbow_joint': 0.00,
+                                  'shoulder_pan_joint': 0.00, 'elbow_joint': 2.00,
+                                  'shoulder_lift_joint': -0.58, 'wrist_3_joint': 0.00,
+                                  'shoulder_lift_joint': -1.25, 'wrist_1_joint': -0.733,
+                                  'wrist_2_joint': 1.5708, 'wrist_3_joint': 0.00}
+
+        arm_joints_target_no_id = arm_joints_target
+        arm_joints_target = {}
+        for key, value in arm_joints_target_no_id.items():
+            arm_joints_target[self.arm_id + '_' + key] = value
+        
+        hand_and_arm_joints_target = dict(hand_joints_target.items() + arm_joints_target.items())
+        print('hand and arm targets')
+        print(hand_and_arm_joints_target)
+
+        self.robot_commander.move_to_joint_value_target_unsafe(hand_and_arm_joints_target, 6.0, True)
+
+        final_hand_and_arm_joint_values = self.robot_commander.get_current_state()
+
+        joint_value_diff_arm_and_hand = self.joints_error_check(hand_and_arm_joints_target, final_hand_and_arm_joint_values)
+
+        self.assertAlmostEqual(joint_value_diff_arm_and_hand, 0, delta=0.2)
+
+        print('arm and hand diff')
+        print(joint_value_diff_arm_and_hand)
 
 if __name__ == '__main__':
 #initialising and setting everything up
     test = TestHandAndArmSim()
     rospy.sleep(10)
-    test.test_hand()
-    rospy.sleep(5)
-    test.test_arm()
+    # test.test_hand()
+    # rospy.sleep(5)
+    # test.test_arm()
+    test.test_hand_and_arm()
