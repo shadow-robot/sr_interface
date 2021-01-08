83f4521d513a0f7a3a70def7f98014454ad8be57
Georgia Blanco-Litchfield <georgia@shadowrobot.com>
2021-01-08 12:11:53 +0000

    updating state saver for more options (#589)
    * updating state saver for more options
    
    * updating
    
    * fixes
    
    * cleaning
    
    * updating state saver
    
    * updating state saver

--- sr_robot_commander/src/sr_robot_commander/sr_robot_state_saver.py
+++ sr_robot_commander/src/sr_robot_commander/sr_robot_state_saver.py
@@ -29,72 +29,72 @@ from threading import Lock
 
 
 class SrStateSaverUnsafe(object):
-    def __init__(self, name, hand_or_arm="both", hand_h=False, save_target=False):
+    def __init__(self, name, hand_or_arm="both", side="right", save_target=False):
 
         self._save = rospy.ServiceProxy(
             'save_robot_state', SaveState)
 
         self._name = name
-        self._hand_h = hand_h
         self._save_target = save_target
 
         if name is None or name == '':
             raise ValueError("Cannot save with empty name.")
 
+        prefix = ""
+        double_error = []
         if hand_or_arm == "arm":
-            self._commander = SrArmCommander()
-            if not self._commander.arm_found():
-                raise ValueError("'No arm found.'")
-
+            if side != "bimanual":
+                prefix = "ra" if side == "right" else "la"
+                self._commander = SrArmCommander(side + '_arm')
+                if not self._commander.arm_found():
+                    double_error.append("Group " + side + "_arm not found.")
+            else:
+                self._commander = SrRobotCommander('two_arms')
         elif hand_or_arm == 'hand':
-            self._commander = SrHandCommander()
-        else:
-            double_error = []
-            try:
-                self._commander = SrHandCommander()
-            except Exception as e:
-                double_error.append(str(e))
+            if side != "bimanual":
+                prefix = "rh" if side == "right" else "lh"
+                self._commander = SrHandCommander(side + '_hand')
+            else:
+                self._commander = SrRobotCommander('two_hands')
+        elif hand_or_arm == 'both':
+            if side != "bimanual":
+                self._commander = SrRobotCommander(side + '_arm_and_hand')
+            else:
+                self._commander = SrRobotCommander('two_arms_and_hands')
 
-            self._arm_commander = SrArmCommander()
-
-            if not self._arm_commander.arm_found():
-                double_error.append("'No arm found.'")
-
-            if len(double_error) != 0:
-                raise ValueError(" ".join(double_error))
+        if len(double_error) != 0:
+            raise ValueError(" ".join(double_error))
 
         self._save_hand = (hand_or_arm == "hand" or hand_or_arm == "both")
         self._save_arm = (hand_or_arm == "arm" or hand_or_arm == "both")
+        self._save_bimanual = (side == 'bimanual')
 
         if save_target:
             rospy.loginfo("Saving targets instead of current values")
             self._mutex = Lock()
             self._target_values = dict()
             if self._save_hand:
-                prefix = "H1" if hand_h else "rh"
                 self._hand_subscriber = rospy.Subscriber("/" + prefix + "_trajectory_controller/state",
                                                          JointTrajectoryControllerState, self._target_cb)
             if self._save_arm:
-                self._arm_subscriber = rospy.Subscriber("/ra_trajectory_controller/state",
+                self._arm_subscriber = rospy.Subscriber("/" + prefix + "_trajectory_controller/state",
                                                         JointTrajectoryControllerState, self._target_cb)
-
-        self._hand_or_arm = hand_or_arm
-
-        rs = RobotState()
+            if self._save_bimanual:
+                if self._save_hand:
+                    self.r_hand_subscriber = rospy.Subscriber("/ra_trajectory_controller/state",
+                                                              JointTrajectoryControllerState, self._target_cb)
+                    self.l_hand_subscriber = rospy.Subscriber("/la_trajectory_controller/state",
+                                                              JointTrajectoryControllerState, self._target_cb)
+                if self._save_arm:
+                    self.r_arm_subscriber = rospy.Subscriber("/ra_trajectory_controller/state",
+                                                             JointTrajectoryControllerState, self._target_cb)
+                    self.l_arm_subscriber = rospy.Subscriber("/la_trajectory_controller/state",
+                                                             JointTrajectoryControllerState, self._target_cb)
 
         current_dict = {}
-
         rospy.loginfo("Getting current")
-
-        if self._hand_or_arm == "both":
-            current_dict = self._commander.get_robot_state_bounded()
-            robot_name = self._commander.get_robot_name()
-        elif self._hand_or_arm == "arm" or self._hand_or_arm == "hand":
-            current_dict = self._commander.get_current_state_bounded()
-            robot_name = self._commander.get_robot_name()
-        else:
-            rospy.logfatal("Unknown save type")
-            exit(-1)
+        current_dict = self._commander.get_current_state()
+        robot_name = self._commander.get_robot_name()
 
         if self._save_target:
             rospy.loginfo("Getting targets")
@@ -115,15 +115,19 @@ class SrStateSaverUnsafe(object):
         if rospy.is_shutdown():
             exit(0)
 
-        rospy.loginfo(current_dict)
-        rs.joint_state = JointState()
-        rs.joint_state.name = current_dict.keys()
-        rs.joint_state.position = current_dict.values()
-        rospy.logwarn(rs)
-        self._save(self._name, robot_name, rs)
+        self.save_state(current_dict, robot_name)
 
     def _target_cb(self, data):
         self._mutex.acquire()
         for n, joint in enumerate(data.joint_names):
             self._target_values[joint] = data.desired.positions[n]
         self._mutex.release()
+
+    def save_state(self, current_dict, robot_name):
+        rs = RobotState()
+        rospy.loginfo(current_dict)
+        rs.joint_state = JointState()
+        rs.joint_state.name = current_dict.keys()
+        rs.joint_state.position = current_dict.values()
+        rospy.logwarn(rs)
+        self._save(self._name, robot_name, rs)
