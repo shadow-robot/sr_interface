be308ec52a2fcf6654f6254e91cc01eeff7a1ac8
mikramarc <michal@shadowrobot.com>
2021-05-28 14:47:58 +0000

    clwaning

--- sr_multi_moveit/sr_multi_moveit_config/scripts/generate_moveit_config.py
+++ sr_multi_moveit/sr_multi_moveit_config/scripts/generate_moveit_config.py
@@ -275,32 +275,19 @@ def generate_kinematics(robot, robot_config, hand_template_path="kinematics_temp
             for group in robot.groups:
                 group_name = group.name
 
-                if group_name in arm_yamldoc_extra_groups:
-                    kinematics_config = arm_yamldoc_extra_groups[group_name]
-                    if prefix:
-                        if "tip_name" in kinematics_config:
-                            tip_name = kinematics_config["tip_name"]
-                            kinematics_config["tip_name"] = prefix + tip_name
-                        if "root_name" in kinematics_config:
-                            root_name = kinematics_config["root_name"]
-                            kinematics_config["root_name"] = prefix + root_name
-
-                    output_str += group.name + ":\n"
-                    output_str += yaml_reindent(yaml.dump(kinematics_config,
-                                                          default_flow_style=False,
-                                                          allow_unicode=True), 2)
-                    output_str += "\n"
-
                 if group_name == manipulator.arm.internal_name:
                     group_name = manipulator.arm.main_group
                     group_prefix = prefix
                 elif manipulator.arm.internal_name in group_name:
                     group_prefix, group_name = group_name.split("_", 1)
                     group_prefix = prefix
+                elif group_name.startswith("two"):
+                    group_name = group.name
+                    group_prefix = prefix
                 else:
                     group_name = group.name[len(prefix):]
                     group_prefix = group.name[:len(prefix)]
-
+ 
                 if group_name in arm_yamldoc and group_prefix == prefix:
                     kinematics_config = arm_yamldoc[group_name]
                     if prefix:
@@ -331,6 +318,7 @@ def generate_kinematics(robot, robot_config, hand_template_path="kinematics_temp
                                                           default_flow_style=False,
                                                           allow_unicode=True), 2)
                     output_str += "\n"
+            rospy.logwarn(output_str)
 
         if manipulator.has_hand:
             # open hand template files
