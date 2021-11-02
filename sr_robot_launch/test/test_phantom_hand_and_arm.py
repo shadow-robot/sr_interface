37d175b216b4ba0321917949bd30bdc89f0e636b
Fotios <fotios@shadowrobot.com>
2021-11-02 16:51:13 +0000

    gixing lint

--- sr_robot_launch/test/test_phantom_hand_and_arm.py
+++ sr_robot_launch/test/test_phantom_hand_and_arm.py
@@ -31,6 +31,7 @@ from actionlib_msgs.msg import GoalStatusArray
 from sensor_msgs.msg import JointState
 from multiprocessing import Process
 
+
 class TestHandAndArmSim(TestCase):
     """
     Tests the Hand and Arm in Sim
@@ -39,7 +40,6 @@ class TestHandAndArmSim(TestCase):
     def setUpClass(cls):
         rospy.wait_for_message('/move_group/status', GoalStatusArray)
         cls.hand_type = 'hand_e'
-      
         cls.robot_commander = SrRobotCommander(name="two_arms_and_hands")
         cls.rh_commander = SrHandCommander(name='right_hand')
         cls.lh_commander = SrHandCommander(name='left_hand')
@@ -48,7 +48,7 @@ class TestHandAndArmSim(TestCase):
 
         cls.arm_commander.set_max_velocity_scaling_factor(0.2)
         cls.robot_commander.set_max_velocity_scaling_factor(0.2)
-        
+
         rospy.sleep(3)
 
     @classmethod
@@ -97,25 +97,25 @@ class TestHandAndArmSim(TestCase):
                 hand_in_config = True
         self.assertTrue(hand_in_config)
 
-    def test_3_joint_state_topic(self):     
+    def test_3_joint_state_topic(self):
         joint_state = rospy.wait_for_message('/joint_states', JointState)
         joints = len(joint_state.name)
         self.assertEqual(joints, 60)
 
     def test_4_joint_state_prefix_right_hand(self):
-        joint_state_exists = self.check_topic_prefix('rh_')     
+        joint_state_exists = self.check_topic_prefix('rh_')
         self.assertTrue(joint_state_exists)
 
     def test_5_joint_state_prefix_left_hand(self):
-        joint_state_exists = self.check_topic_prefix('lh_')     
+        joint_state_exists = self.check_topic_prefix('lh_')
         self.assertTrue(joint_state_exists)
 
     def test_6_joint_state_prefix_right_arm(self):
-        joint_state_exists = self.check_topic_prefix('ra_')     
+        joint_state_exists = self.check_topic_prefix('ra_')
         self.assertTrue(joint_state_exists)
 
     def test_7_joint_state_prefix_left_arm(self):
-        joint_state_exists = self.check_topic_prefix('la_')     
+        joint_state_exists = self.check_topic_prefix('la_')
         self.assertTrue(joint_state_exists)
 
     def test_8_left_hand(self):
@@ -125,27 +125,27 @@ class TestHandAndArmSim(TestCase):
         goal_status = goal_status.status_list[0].status
         self.assertEqual(goal_status, 3)
 
-    def test_9_right_hand(self):        
+    def test_9_right_hand(self):
         self.lh_commander.move_to_named_target('pack', wait=True)
         rospy.sleep(2)
         goal_status = rospy.wait_for_message('/move_group/status', GoalStatusArray)
         goal_status = goal_status.status_list[0].status
         self.assertEqual(goal_status, 3)
 
-    def test_10_joint_states(self):     
+    def test_10_joint_states(self):
         joint_state = rospy.wait_for_message('/joint_states', JointState)
         joints = len(joint_state.name)
         self.assertEqual(joints, 60)
- 
+
     def test_11_arms(self):
 
         la_arm_joints_target = {'la_shoulder_pan_joint': 0.00, 'la_elbow_joint': -1.43,
-                                 'la_shoulder_lift_joint': -1.82, 'la_wrist_1_joint': 3.24,
-                                 'la_wrist_2_joint': -1.57, 'la_wrist_3_joint': 3.13}
+                                'la_shoulder_lift_joint': -1.82, 'la_wrist_1_joint': 3.24,
+                                'la_wrist_2_joint': -1.57, 'la_wrist_3_joint': 3.13}
         ra_arm_joints_target = {'ra_shoulder_pan_joint': 0.00, 'ra_elbow_joint': 1.43,
-                                 'ra_shoulder_lift_joint': -1.27, 'ra_wrist_1_joint': -0.1,
-                                 'ra_wrist_2_joint': 1.57, 'ra_wrist_3_joint': 3.13}
-        arm_joints_target = dict(la_arm_joints_target.items() | ra_arm_joints_target.items())
+                                'ra_shoulder_lift_joint': -1.27, 'ra_wrist_1_joint': -0.1,
+                                'ra_wrist_2_joint': 1.57, 'ra_wrist_3_joint': 3.13}
+        arm_joints_target = dict(la_arm_joints_target.iteritems() | ra_arm_joints_target.iteritems())
 
         self.arm_commander.move_to_joint_value_target(arm_joints_target, wait=True)
         rospy.sleep(5)
@@ -154,20 +154,20 @@ class TestHandAndArmSim(TestCase):
         expected_and_final_joint_value_diff_arm = self.joints_error_check(arm_joints_target, final_arm_joint_values)
 
         self.assertAlmostEqual(expected_and_final_joint_value_diff_arm, 0, delta=0.2)
-        
+
     def test_12_hand_and_arm(self):
         hand_joints_target = self.hand_commander.get_current_state()
 
         ra_arm_joints_target = {'ra_shoulder_pan_joint': 0.00, 'ra_elbow_joint': 2.0,
-                                 'ra_shoulder_lift_joint': -1.25, 'ra_wrist_1_joint': -0.733,
-                                 'ra_wrist_2_joint': 1.578, 'ra_wrist_3_joint': -3.1416}
+                                'ra_shoulder_lift_joint': -1.25, 'ra_wrist_1_joint': -0.733,
+                                'ra_wrist_2_joint': 1.578, 'ra_wrist_3_joint': -3.1416}
         la_arm_joints_target = {'la_shoulder_pan_joint': 0.0, 'la_elbow_joint': -2.0,
-                                 'la_shoulder_lift_joint': -1.89, 'la_wrist_1_joint': 3.8,
-                                 'la_wrist_2_joint': -1.5708, 'la_wrist_3_joint': 3.1416}
+                                'la_shoulder_lift_joint': -1.89, 'la_wrist_1_joint': 3.8,
+                                'la_wrist_2_joint': -1.5708, 'la_wrist_3_joint': 3.1416}
 
-        arm_joints_target = dict(ra_arm_joints_target.items() | la_arm_joints_target.items())
+        arm_joints_target = dict(ra_arm_joints_target.iteritems() | la_arm_joints_target.iteritems())
 
-        hand_and_arm_joints_target = dict(hand_joints_target.items() | arm_joints_target.items())
+        hand_and_arm_joints_target = dict(hand_joints_target.iteritems() | arm_joints_target.iteritems())
         self.robot_commander.move_to_joint_value_target_unsafe(hand_and_arm_joints_target, 10.0, True)
 
         rospy.sleep(5)
