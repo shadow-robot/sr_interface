c74a749df27e5e10a9451457039f11413606a043
Fotios <fotios@shadowrobot.com>
2021-11-16 12:23:41 +0000

    fixing sim tests

--- sr_robot_launch/test/test_hand_and_arm_sim.py
+++ sr_robot_launch/test/test_hand_and_arm_sim.py
@@ -54,12 +54,12 @@ class TestHandAndArmSim(TestCase):
 
         if cls.hand_id == 'rh':
             cls.arm_id = 'ra'
-            cls.robot_commander = SrRobotCommander(name="right_arm_and_hand", set_ground=not(cls.scene))
+            cls.robot_commander = SrRobotCommander(name="right_arm_and_hand")
             cls.hand_commander = SrHandCommander(name='right_hand')
             cls.arm_commander = SrArmCommander(name='right_arm', set_ground=not(cls.scene))
         elif cls.hand_id == 'lh':
             cls.arm_id = 'la'
-            cls.robot_commander = SrRobotCommander(name="left_arm_and_hand", set_ground=not(cls.scene))
+            cls.robot_commander = SrRobotCommander(name="left_arm_and_hand")
             cls.hand_commander = SrHandCommander(name='left_hand')
             cls.arm_commander = SrArmCommander(name='left_arm', set_ground=not(cls.scene))
 
