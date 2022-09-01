69aced15e7ebcc75d459a47c6b871ae0a0bdbca2
Jesus Ferrandiz <jeferrandiz98@gmail.com>
2022-09-01 08:43:45 +0000

    setting joint_value_target as the current state for the joints that are not specified

--- sr_robot_commander/src/sr_robot_commander/sr_robot_commander.py
+++ sr_robot_commander/src/sr_robot_commander/sr_robot_commander.py
@@ -285,6 +285,7 @@ class SrRobotCommander(object):
             self._move_group_commander.set_start_state_to_current_state()
         else:
             self._move_group_commander.set_start_state(custom_start_state)
+        self._move_group_commander.set_joint_value_target(self._move_group_commander.get_current_joint_values())
         self._move_group_commander.set_joint_value_target(joint_states_cpy)
         self.__plan = self._move_group_commander.plan()[CONST_TUPLE_TRAJECTORY_INDEX]
         return self.__plan
