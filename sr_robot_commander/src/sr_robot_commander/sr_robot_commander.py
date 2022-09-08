c30d2ec231bd8193c2e92debc44435982355850b
Jesus Ferrandiz <jeferrandiz98@gmail.com>
2022-09-08 13:04:21 +0000

    removed print

--- sr_robot_commander/src/sr_robot_commander/sr_robot_commander.py
+++ sr_robot_commander/src/sr_robot_commander/sr_robot_commander.py
@@ -289,7 +289,6 @@ class SrRobotCommander(object):
         self._move_group_commander.set_joint_value_target(self.get_current_state_bounded())
         self._move_group_commander.set_joint_value_target(joint_states_cpy)
         self.__plan = self._move_group_commander.plan()[CONST_TUPLE_TRAJECTORY_INDEX]
-        print(self.__plan)
         return self.__plan
 
     def check_plan_is_valid(self):
