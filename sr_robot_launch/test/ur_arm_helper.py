ac4164e9fbe5a9f59f96af8ef1a2e33862f78160
devops-shadow <111312916+devops-shadow@users.noreply.github.com>
2022-09-11 22:32:17 +0100

    Update ur_arm_helper.py

--- sr_robot_launch/test/ur_arm_helper.py
+++ sr_robot_launch/test/ur_arm_helper.py
@@ -18,13 +18,14 @@ import rospy
 from ur_dashboard_msgs.srv import GetSafetyMode, GetProgramState, GetRobotMode, Load, IsProgramRunning
 from std_srvs.srv import Trigger
 
+
 def resend_program():
     try:
         service_call = rospy.ServiceProxy('/la_sr_ur_robot_hw/dashboard/program_running', IsProgramRunning)
         response_left = service_call()
-        if response_left.program_running==False:
+        if response_left.program_running is False:
             print("Resending program for left arm")
-            service_call = rospy.ServiceProxy('/la_sr_ur_robot_hw/resend_robot_program', Trigger)                    
+            service_call = rospy.ServiceProxy('/la_sr_ur_robot_hw/resend_robot_program', Trigger)
             response_left = service_call()
     except rospy.ServiceException as e:
         rospy.logerr("Service call failed for left arm %s", e)
@@ -32,13 +33,14 @@ def resend_program():
     try:
         service_call = rospy.ServiceProxy('/ra_sr_ur_robot_hw/dashboard/program_running', IsProgramRunning)
         response_right = service_call()
-        if response_right.program_running==False:
+        if response_right.program_running is False:
             print("Resending program for right arm")
             service_call = rospy.ServiceProxy('/ra_sr_ur_robot_hw/resend_robot_program', Trigger)
             response_right = service_call()
     except rospy.ServiceException as e:
         rospy.logerr("Service call failed for right arm %s", e)
         raise
+
 
 if __name__ == "__main__":
     rospy.init_node("ur_arm_test_helper")
