48db50ef3c4fe110b82acf7c6108515f20b7cdf7
devops-shadowrobot <33877923+devops-shadowrobot@users.noreply.github.com>
2022-10-13 10:16:23 +0100

    Cleaningup (#772)
    * Delete Dockerfile
    
    * Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros environment
-source "/workspace/shadow_robot/base/devel/setup.bash"
-
-roscore &
-
-rosrun sr_moveit_planner_benchmarking benchmark_planners.py _data:=`rospack find sr_moveit_planner_benchmarking`/data _results:=/results
