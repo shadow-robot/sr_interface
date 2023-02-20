shadow-robot/sr_interface

commit c9d39a4dc166b13409584ecdbf63c3126758d19a

B #tqc 415 multi states (#807)
* Adding "hand change" and "pack in box" saved states for UR10e arms.

* Changes for N hands (e.g. two arms one hand)

* Fixing bug introduced by lint changes

* Lint

* Clarifying and fixing typo/bug in multi-robot move group state generation

sr_multi_moveit/sr_multi_moveit_config/scripts/generate_robot_srdf.py
@@ -160,17 +160,6 @@ class SRDFRobotGenerator:
         if description_file is None and len(sys.argv) > 1:
             description_file = sys.argv[1]
 
-        # If specified, load the combined robot move group joint states from the config file
-        # These are states that span multiple robots, so can't de defined in the individual robot SRDFs
-        self._multi_robot_move_group_states = {}
-        if len(sys.argv) > 2:
-            try:
-                with open(sys.argv[2], "r", encoding="utf-8") as stream:
-                    self._multi_robot_move_group_states = yaml.safe_load(stream)
-            except FileNotFoundError:
-                rospy.logwarn(f'Could not open the specified move group saved states definition file: '
-                              f'"{sys.argv[2]}". No move group saved states loaded.')
-
         self._save_files = rospy.get_param('~save_files', False)
         self._path_to_save_files = rospy.get_param('~path_to_save_files', "/tmp/")
         self._file_name = rospy.get_param('~file_name', "generated_robot")
@@ -214,20 +203,10 @@ class SRDFRobotGenerator:
 
         # Add groups for bimanual arm and hand systems
         if len(self.robot.manipulators) == 2:
-            if self.robot.manipulators[0].has_arm and self.robot.manipulators[1].has_arm:
-                if self.robot.manipulators[0].has_hand and self.robot.manipulators[1].has_hand:
-                    comment = ["Bimanual groups with hands"]
-                    self.add_bimanual_arm_groups(self.robot.manipulators[0].arm.internal_name,
-                                                 self.robot.manipulators[1].arm.internal_name)
-                else:
-                    comment = ["Bimanual groups without hands"]
-                    self.add_bimanual_arm_groups(self.robot.manipulators[0].arm.internal_name,
-                                                 self.robot.manipulators[1].arm.internal_name,
-                                                 False)
-                self.add_comments(comment)
+            self.add_bimanual_arm_groups(self.robot.manipulators)
 
             if self.robot.manipulators[0].has_hand and self.robot.manipulators[1].has_hand:
-                comment = ["Bimanual groups with hands"]
+                comment = ["Bimanual hand groups"]
                 self.add_bimanual_hand_groups(self.robot.manipulators[0].hand.internal_name,
                                               self.robot.manipulators[1].hand.internal_name)
                 self.add_comments(comment)
@@ -259,7 +238,7 @@ class SRDFRobotGenerator:
             if manipulator.has_hand:
                 self.parse_hand_collisions(manipulator_id, manipulator)
 
-        # Add the config-file-defined multi-robot move group states
+        # Generate and add the multi-robot move group states
         self.add_multi_robot_move_group_states()
 
         # Finish and close file
@@ -404,8 +383,6 @@ class SRDFRobotGenerator:
     # Get move group states from an XML DOM, returning a dictionary of {move_group: {state: values}
     @staticmethod
     def parse_move_group_states(srdf_xml_dom, group_states):
-        if not group_states:
-            group_states = {}
         for group_state_xml in srdf_xml_dom.getElementsByTagName("group_state"):
             group = group_state_xml.getAttribute("group")
             state_name = group_state_xml.getAttribute("name")
@@ -418,12 +395,23 @@ class SRDFRobotGenerator:
                         group_state_child.getAttribute("value")
         return group_states
 
-    # Generates states for move groups that span multiple robots, and therefore can't be defined in their SRDFs
+    # Generates states for move groups that span multiple robots, and therefore can't be defined in individual robot
+    # SRDFs (xacros). These states can inherit from single-robot move group states, or each other.
     def add_multi_robot_move_group_states(self):
-        # Check which move groups are in the generated SRDF
+        # The YAML file containing the multi-robot move group state definitions is an optional argument to this script;
+        # if it's not provided, no multi-robot move group states will be generated.
+        self._multi_robot_move_group_states = {}
+        if len(sys.argv) > 2:
+            try:
+                with open(sys.argv[2], "r", encoding="utf-8") as stream:
+                    self._multi_robot_move_group_states = yaml.safe_load(stream)
+            except FileNotFoundError:
+                rospy.logwarn(f'Could not open the specified multi-robot move group states definition file: '
+                              f'"{sys.argv[2]}". No multi-robot group states loaded.')
+        # Check which move groups exist by parsing the previously-generated SRDF
         self.new_robot_srdf.seek(0)
         move_group_names = list(set(re.findall(r'<group\s+.*name="([^"]*)"', self.new_robot_srdf.read())))
-        # Collect the move group states defined in the separate robot SRDFs
+        # Collect the single-robot move group states defined in the separate robot SRDFs
         self._single_robot_move_group_states = {}
         for hand_xml in self.hand_srdf_xmls:
             SRDFRobotGenerator.parse_move_group_states(hand_xml, self._single_robot_move_group_states)
@@ -492,31 +480,52 @@ class SRDFRobotGenerator:
             combined_move_group_state["joint_angles"]
         return True
 
-    def add_bimanual_arm_groups(self, group_1, group_2, hands=True):
+    def add_bimanual_arm_groups(self, manipulators):
+        self.add_comments(comments=["Bimanual arm groups without hands"])
+        # Add two arms (no hands) group
+        self.add_move_group_combining_others(
+            'two_arms', [manipulators[0].arm.internal_name, manipulators[1].arm.internal_name])
+        if manipulators[0].has_hand or manipulators[1].has_hand:
+            self.add_comments(comments=["Bimanual arm groups with hand(s)"])
+        if manipulators[0].has_hand:
+            # Add two arms and first hand group
+            self.add_move_group_combining_others(
+                f'two_arms_and_{manipulators[0].hand.internal_name}',
+                [f'{manipulators[0].arm.internal_name}_and_hand', f'{manipulators[1].arm.internal_name}'])
+            if not manipulators[0].hand.is_lite:
+                # Add two arms and first hand wrist group
+                self.add_move_group_combining_others(
+                    f'two_arms_and_{manipulators[0].side}_wrist',
+                    [f'{manipulators[0].arm.internal_name}_and_wrist', f'{manipulators[1].arm.internal_name}'])
+        if manipulators[1].has_hand:
+            # Add two arms and second hand group
+            self.add_move_group_combining_others(
+                f'two_arms_and_{manipulators[1].hand.internal_name}',
+                [f'{manipulators[1].arm.internal_name}_and_hand', f'{manipulators[0].arm.internal_name}'])
+            if not manipulators[1].hand.is_lite:
+                # Add two arms and second hand wrist group
+                self.add_move_group_combining_others(
+                    f'two_arms_and_{manipulators[1].side}_wrist',
+                    [f'{manipulators[1].arm.internal_name}_and_wrist', f'{manipulators[0].arm.internal_name}'])
+        if manipulators[0].has_hand and manipulators[1].has_hand:
+            # Add two arms and two hands group
+            self.add_move_group_combining_others(
+                'two_arms_and_hands',
+                [f'{manipulators[0].arm.internal_name}_and_hand', f'{manipulators[1].arm.internal_name}_and_hand'])
+            if (not manipulators[0].hand.is_lite) and (not manipulators[1].hand.is_lite):
+                # Add two arms and two wrists group
+                self.add_move_group_combining_others(
+                    'two_arms_and_wrists',
+                    [f'{manipulators[1].arm.internal_name}_and_wrist',
+                        f'{manipulators[0].arm.internal_name}_and_wrist'])
+
+    def add_move_group_combining_others(self, new_group_name, existing_group_names=None):
+        """ Adds a new move group to the SRDF that includes other, existing move groups. """
         new_group = xml.dom.minidom.Document().createElement('group')
-        new_group.setAttribute("name", "two_arms")
-        arm_group_1 = xml.dom.minidom.Document().createElement('group name="' + group_1 + '"')
-        new_group.appendChild(arm_group_1)
-        arm_group_2 = xml.dom.minidom.Document().createElement('group name="' + group_2 + '"')
-        new_group.appendChild(arm_group_2)
+        new_group.setAttribute("name", new_group_name)
+        for existing_group_name in existing_group_names:
+            new_group.appendChild(xml.dom.minidom.Document().createElement(f'group name="{existing_group_name}"'))
         new_group.writexml(self.new_robot_srdf, indent="  ", addindent="  ", newl="\n")
-
-        if hands:
-            new_group = xml.dom.minidom.Document().createElement('group')
-            new_group.setAttribute("name", "two_arms_and_hands")
-            arm_group_1 = xml.dom.minidom.Document().createElement('group name="' + group_1 + '_and_hand"')
-            arm_group_2 = xml.dom.minidom.Document().createElement('group name="' + group_2 + '_and_hand"')
-            new_group.appendChild(arm_group_1)
-            new_group.appendChild(arm_group_2)
-            new_group.writexml(self.new_robot_srdf, indent="  ", addindent="  ", newl="\n")
-
-            new_group = xml.dom.minidom.Document().createElement('group')
-            new_group.setAttribute("name", "two_arms_and_wrists")
-            arm_group_1 = xml.dom.minidom.Document().createElement('group name="' + group_1 + '_and_wrist"')
-            arm_group_2 = xml.dom.minidom.Document().createElement('group name="' + group_2 + '_and_wrist"')
-            new_group.appendChild(arm_group_1)
-            new_group.appendChild(arm_group_2)
-            new_group.writexml(self.new_robot_srdf, indent="  ", addindent="  ", newl="\n")
 
     def add_bimanual_hand_groups(self, group_1, group_2):
         new_group = xml.dom.minidom.Document().createElement('group')

sr_multi_moveit/sr_multi_moveit_config/config/multi_robot_move_group_states.yaml
@@ -34,11 +34,34 @@ left_arm:  # Must be an existing move group; ignored if not.
       la_wrist_1_joint: -2.4
       la_wrist_2_joint: -1.5708
       la_wrist_3_joint: 3.1416
+  hand_change:
+    joint_angles:
+      la_elbow_joint: -1.862694263458252
+      la_shoulder_lift_joint: -2.2352358303465785
+      la_shoulder_pan_joint: -0.0050919691668909195
+      la_wrist_1_joint: -0.6089532536319275
+      la_wrist_2_joint: -1.5779951254474085
+      la_wrist_3_joint: 3.141533851623535
+  pack_in_box:
+    joint_angles:
+      la_shoulder_pan_joint: 3.14159
+      la_shoulder_lift_joint: -1.5708
+      la_elbow_joint: -2.87979
+      la_wrist_1_joint: -1.8326
+      la_wrist_2_joint: 3.14159
+      la_wrist_3_joint: 3.14159
 left_arm_and_wrist:
   la_start:
     inherit_from:  # States can inherit from other states, which must be defined either in this file or in a robot SRDF.
       - move_group: left_arm
         move_group_state: la_start  # Defined in this file.
+    joint_angles:
+      lh_WRJ1: 0.0
+      lh_WRJ2: 0.0
+  hand_change:
+    inherit_from:
+      - move_group: left_arm
+        move_group_state: hand_change
     joint_angles:
       lh_WRJ1: 0.0
       lh_WRJ2: 0.0
@@ -49,6 +72,12 @@ left_arm_and_hand:
         move_group_state: la_start
       - move_group: left_hand
         move_group_state: relaxed  # Defined in the robot SRDF.
+  hand_change:
+    inherit_from:
+      - move_group: left_arm_and_wrist
+        move_group_state: hand_change
+      - move_group: left_hand
+        move_group_state: pack
 right_arm:
   ra_start:
     joint_angles:
@@ -58,11 +87,34 @@ right_arm:
       ra_wrist_1_joint: -0.733
       ra_wrist_2_joint: 1.5708
       ra_wrist_3_joint: -3.1416
+  hand_change:
+    joint_angles:
+      ra_elbow_joint: 1.8135
+      ra_shoulder_lift_joint: -0.8813
+      ra_shoulder_pan_joint: -0.0013
+      ra_wrist_1_joint: -2.484
+      ra_wrist_2_joint: 1.573
+      ra_wrist_3_joint: -3.1416
+  pack_in_box:
+    joint_angles:
+      ra_shoulder_pan_joint: 3.14159
+      ra_shoulder_lift_joint: -1.5708
+      ra_elbow_joint: -2.87979
+      ra_wrist_1_joint: -1.8326
+      ra_wrist_2_joint: 3.14159
+      ra_wrist_3_joint: 3.14159
 right_arm_and_wrist:
   ra_start:
     inherit_from:
       - move_group: right_arm
         move_group_state: ra_start
+    joint_angles:
+      rh_WRJ1: 0.0
+      rh_WRJ2: 0.0
+  hand_change:
+    inherit_from:
+      - move_group: right_arm
+        move_group_state: hand_change
     joint_angles:
       rh_WRJ1: 0.0
       rh_WRJ2: 0.0
@@ -73,6 +125,12 @@ right_arm_and_hand:
         move_group_state: ra_start
       - move_group: right_hand
         move_group_state: relaxed
+  hand_change:
+    inherit_from:
+      - move_group: right_arm_and_wrist
+        move_group_state: hand_change
+      - move_group: right_hand
+        move_group_state: pack
 two_arms:
   start:
     inherit_from:
@@ -80,20 +138,12 @@ two_arms:
         move_group_state: ra_start
       - move_group: left_arm
         move_group_state: la_start
-two_arms_and_wrists:
-  start:
+  hand_change:
     inherit_from:
-      - move_group: right_arm_and_wrist
-        move_group_state: ra_start
-      - move_group: left_arm_and_wrist
-        move_group_state: la_start
-two_arms_and_hands:
-  start:
-    inherit_from:
-      - move_group: right_arm_and_hand
-        move_group_state: ra_start
-      - move_group: left_arm_and_hand
-        move_group_state: la_start
+      - move_group: right_arm
+        move_group_state: hand_change
+      - move_group: left_arm
+        move_group_state: hand_change
 two_hands:
   open:
     inherit_from:
@@ -119,3 +169,29 @@ two_hands:
         move_group_state: relaxed
       - move_group: left_hand
         move_group_state: relaxed
+two_arms_and_wrists:
+  start:
+    inherit_from:
+      - move_group: right_arm_and_wrist
+        move_group_state: ra_start
+      - move_group: left_arm_and_wrist
+        move_group_state: la_start
+  hand_change:
+    inherit_from:
+      - move_group: right_arm_and_wrist
+        move_group_state: hand_change
+      - move_group: left_arm_and_wrist
+        move_group_state: hand_change
+two_arms_and_hands:
+  start:
+    inherit_from:
+      - move_group: right_arm_and_hand
+        move_group_state: ra_start
+      - move_group: left_arm_and_hand
+        move_group_state: la_start
+  hand_change:
+    inherit_from:
+      - move_group: right_arm_and_hand
+        move_group_state: hand_change
+      - move_group: left_arm_and_hand
+        move_group_state: hand_change

commit 76fbca4e8abacd50df2d1046b978bacffc6affc6

Fixing bug introduced in #807

sr_multi_moveit/sr_multi_moveit_config/scripts/generate_robot_srdf.py
@@ -203,7 +203,8 @@ class SRDFRobotGenerator:
 
         # Add groups for bimanual arm and hand systems
         if len(self.robot.manipulators) == 2:
-            self.add_bimanual_arm_groups(self.robot.manipulators)
+            if (self.robot.manipulators[0].has_arm and self.robot.manipulators[1].has_arm):
+                self.add_bimanual_arm_groups(self.robot.manipulators)
 
             if self.robot.manipulators[0].has_hand and self.robot.manipulators[1].has_hand:
                 comment = ["Bimanual hand groups"]